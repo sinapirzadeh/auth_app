a5af12ff61519955f43c60afa9e04f03a53d9e80
Sina Pirzadeh <104755076+sinapirzadeh@users.noreply.github.com>
2023-11-14 20:07:51 +0330

    Update views.py

--- dashboard/api/views.py
+++ dashboard/api/views.py
@@ -1,29 +1,29 @@
-from rest_framework import generics
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from user.models import CustomUser
 from .serializers import ProfileSerializer
-
-
-class ProfileListAPI(generics.ListAPIView):
-    permission_classes = [IsAuthenticated]
-    queryset = CustomUser.objects.all()
-    serializer_class = ProfileSerializer
-
-
-class ProfileDetailAPI(generics.RetrieveAPIView):
-    permission_classes = [IsAuthenticated]
-    queryset = CustomUser.objects.all()
-    serializer_class = ProfileSerializer
-    lookup_field = 'pk'
+from user.models import CustomUser
+from rest_framework import generics
 
 
 class MyProfileAPI(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        profile = CustomUser.objects.get(pk=request.user.pk)
+        profile = CustomUser.objects.get(pk=request.user.id)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+
+class ProfileDetailAPI(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = CustomUser.objects.all()
+    serializer_class = ProfileSerializer
+    lookup_field = 'id'
+
+
+
+class ProfileListAPI(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = CustomUser.objects.all()
+    serializer_class = ProfileSerializer
